92230a38966fec7e89f5c4cbd54fdff4550ff924
respectwo2 <respectwo@gmail.com>
2024-11-15 16:16:44 +0900

    feat: groupmapper 작성

--- src/main/java/com/example/gocalowly/domain/group/mapper/GroupMapper.java
+++ src/main/java/com/example/gocalowly/domain/group/mapper/GroupMapper.java
@@ -1,5 +1,14 @@
 package com.example.gocalowly.domain.group.mapper;
 
-public class GroupMapper {
+import com.example.gocalowly.domain.group.dto.response.GroupInfoResponseDto;
+import com.example.gocalowly.domain.group.entity.GroupEntity;
 
+public class GroupMapper {
+	
+	public GroupInfoResponseDto entityToDto(GroupEntity group) {
+		return new GroupInfoResponseDto(
+				group.getGroupName(),
+				group.getHeadcount()
+				);
+	}
 }
