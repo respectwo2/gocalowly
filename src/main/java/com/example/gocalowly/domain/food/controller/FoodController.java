eef8721f37011c7daf4416f552fcd977ae40ea36
CinaDor <cinador@github.com>
2024-11-16 23:39:15 +0900

    feat: 음식 검색 기능, 사용자의 목표 칼로리, 오늘 등록 칼로리 조회 기능

---
+++ src/main/java/com/example/gocalowly/domain/food/controller/FoodController.java
@@ -0,0 +1,29 @@
+package com.example.gocalowly.domain.food.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.gocalowly.domain.food.dto.response.DailyGoalStatusResponseDto;
+import com.example.gocalowly.domain.food.dto.response.FoodSearchResultResponseDto;
+import com.example.gocalowly.domain.food.service.FoodService;
+
+@RestController
+@RequestMapping("/api/food")
+public class FoodController {
+	FoodService foodService;
+	
+	public FoodController(FoodService foodService) {
+		this.foodService = foodService;
+	}
+	
+	@GetMapping("/search")
+	public ResponseEntity<List<FoodSearchResultResponseDto>> searchFood(@RequestParam("foodName") String foodName) {
+		// 검색 결과 없을 경우 따로 처리할 수도 있어요
+		return ResponseEntity.ok(foodService.searchFoodByName(foodName));
+	}
+}
