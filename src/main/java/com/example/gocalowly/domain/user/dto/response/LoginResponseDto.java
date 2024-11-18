97cfebb819df93b08b99abe04b0361c642e3ba36
CinaDor <cinador@github.com>
2024-11-18 16:16:57 +0900

    fix: @Component 추가, 사용하지 않는 import 제거

--- src/main/java/com/example/gocalowly/domain/user/dto/response/LoginResponseDto.java
+++ src/main/java/com/example/gocalowly/domain/user/dto/response/LoginResponseDto.java
@@ -1,7 +1,5 @@
 package com.example.gocalowly.domain.user.dto.response;
 
-import java.util.UUID;
-
 //로그인에 성공한 이후 유저넘버와 닉네임을 응답할때 사용되는 DTO
 public class LoginResponseDto {
 	private String userNickname;
