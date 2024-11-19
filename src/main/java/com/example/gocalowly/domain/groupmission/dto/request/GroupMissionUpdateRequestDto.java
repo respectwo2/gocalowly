2df1923bce4d5296a75dc111d984316fcab47c2a
CinaDor <cinador@github.com>
2024-11-19 14:26:44 +0900

    feat: 유저 그룹 미션 업데이트 기능

--- src/main/java/com/example/gocalowly/domain/groupmission/dto/request/GroupMissionUpdateRequestDto.java
+++ src/main/java/com/example/gocalowly/domain/groupmission/dto/request/GroupMissionUpdateRequestDto.java
@@ -2,6 +2,8 @@ package com.example.gocalowly.domain.groupmission.dto.request;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 // 미션 달성 여부 업데이트를 요청하는 RequestDto 입니다. 
 public class GroupMissionUpdateRequestDto {
 	private List<MissionComplete> missionCompletes; // 미션 별 달성 여부 리스트 입니다.
@@ -20,21 +22,22 @@ public class GroupMissionUpdateRequestDto {
 	}
 
 	public static class MissionComplete { // 미션 달성 여부 클래스를 내부에서 선언한 클래스입니다.
-		private int missonId; // 미션 ID
+		private int missionNo; // 미션 ID
+		@JsonProperty("isMissionComplete")
 		private boolean isMissionComplete; // 미션 달성 여부
 
 		public MissionComplete() {
 			// TODO Auto-generated constructor stub
 		}
 
-		public MissionComplete(int missonId, boolean isMissionComplete) {
+		public MissionComplete(int missionNo, boolean isMissionComplete) {
 			super();
-			this.missonId = missonId;
+			this.missionNo = missionNo;
 			this.isMissionComplete = isMissionComplete;
 		}
 
-		public int getMissonId() {
-			return missonId;
+		public int getMissionNo() {
+			return missionNo;
 		}
 
 		public boolean isMissionComplete() {
