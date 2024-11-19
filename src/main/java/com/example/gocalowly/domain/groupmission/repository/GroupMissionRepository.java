194b1f9de1d6c057199f261f996bcc4564e603af
CinaDor <cinador@github.com>
2024-11-19 13:02:10 +0900

    feat: 유저 그룹 미션 불러오기 기능

---
+++ src/main/java/com/example/gocalowly/domain/groupmission/repository/GroupMissionRepository.java
@@ -0,0 +1,8 @@
+package com.example.gocalowly.domain.groupmission.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.gocalowly.domain.groupmission.entity.GroupMissionEntity;
+
+public interface GroupMissionRepository extends JpaRepository<GroupMissionEntity, Integer>{
+}
