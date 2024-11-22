78dcda5d41dbc1c461232b399ab66c29db1ef0a0
CinaDor <cinador@github.com>
2024-11-22 16:32:17 +0900

    feat: 로그인을 제외한 모든 API 요청에서 JWT 검사

--- src/main/java/com/example/gocalowly/domain/user/controller/UserController.java
+++ src/main/java/com/example/gocalowly/domain/user/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.gocalowly.domain.token.controller.TokenController;
+import com.example.gocalowly.domain.token.exception.TokenException;
 import com.example.gocalowly.domain.user.dto.request.LoginRequestDto;
 import com.example.gocalowly.domain.user.dto.request.SignUpRequestDto;
 import com.example.gocalowly.domain.user.dto.request.TargetCalorieRequestDto;
@@ -19,16 +21,20 @@ import com.example.gocalowly.domain.user.entity.UserEntity;
 import com.example.gocalowly.domain.user.service.UserGoalService;
 import com.example.gocalowly.domain.user.service.UserService;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 	UserService userService;
+	TokenController tokenController;
 	private final static UUID TEST_USERID = UUID.fromString("00000000-0000-0000-0000-000000000001");
 	
-	public UserController(UserService userService) {
+	public UserController(UserService userService, TokenController tokenController) {
 		this.userService = userService;
+		this.tokenController = tokenController;
 	}
 	
 	@PostMapping("/target-calorie")
@@ -53,17 +59,21 @@ public class UserController {
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto, HttpSession session) {
+	public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto, HttpSession session, HttpServletResponse response) {
 		try{
 			LoginResponseDto loginResponseDto = userService.loginUser(loginRequestDto);
 			
 			session.setAttribute("userId", loginResponseDto.getUserId());
 			session.setAttribute("groupNo", loginResponseDto.getGroupNo());
 			session.setAttribute("userNickname", loginResponseDto.getUserNickname());
+			
+			tokenController.setTokens(loginResponseDto.getUserId(), response);
 					
 			return ResponseEntity.ok("로그인 되었습니다!");
 		}catch(NoSuchElementException e) {
 			return ResponseEntity.ok("아이디 또는 비밀번호가 잘못되었습니다.");
+		}catch(TokenException e) {
+			return ResponseEntity.ok(e.getMessage()); 
 		}
 	}
 }
