792733ba3ac0ad17fb37a6992fd198a63b216d54
CinaDor <cinador@github.com>
2024-11-24 20:53:35 +0900

    fix: chatgpt에게 보내는 요청 수정

--- src/main/java/com/example/gocalowly/domain/group/controller/AIController.java
+++ src/main/java/com/example/gocalowly/domain/group/controller/AIController.java
@@ -18,9 +18,9 @@ public class AIController {
 	private final GroupService groupService;
 	
 	private static final String REQUEST_TO_GPT = "식단 관리 그룹 참가자들이 모인 한글 그룹 이름 하나 추천해줘. "
-			+ "그룹 이름은 그룹으로 끝나고, 딱딱하지 않고 캐쥬얼한 단어를 썼으면 좋겠어. "
+			+ "그룹 이름은 사용자에게 말하는 듯한 그룹 이름으로 해주고, 딱딱하지 않고 캐쥬얼한 단어를 썼으면 좋겠어. "
 			+ "글자 수는 한글 기준으로 25자 이상이어야 해. "
-			+ "다른 미사여구 없이 딱 그룹 이름만 답변해줬으면 좋겠어.";
+			+ "다른 미사여구, 쌍따옴표 없이 딱 그룹 이름만 답변해줬으면 좋겠어.";
 	
 	public AIController(OpenAiChatModel openAiChatModel, GroupService groupService) {
 		this.openAiChatModel = openAiChatModel;
